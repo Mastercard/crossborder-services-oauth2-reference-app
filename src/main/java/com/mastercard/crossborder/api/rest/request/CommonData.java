d1540f2e041a31cc29a0b3c22fac64058e4cd6e3
e082569 <sonali.sonawale@mastercard.com>
2022-09-13 16:34:15 +0530

    Balance API changes

---
+++ src/main/java/com/mastercard/crossborder/api/rest/request/CommonData.java
@@ -0,0 +1,36 @@
+package com.mastercard.crossborder.api.rest.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+public class CommonData {
+
+    private static final long serialVersionUID = 1L;
+
+    private String transactionReference;
+    private AdditionalField additionalField;
+
+    @JsonProperty(value = "additional_data")
+    @XmlElement(name = "additional_data")
+    public AdditionalField getAdditionalField() {
+        return additionalField;
+    }
+
+    public void setAdditionalField(AdditionalField additionalField) {
+        this.additionalField = additionalField;
+    }
+
+    @JsonProperty(value = "TransactionReference")
+    @XmlElement(name = "TransactionReference", required = true)
+    public String getTransactionReference() {
+        return transactionReference;
+    }
+
+    public void setTransactionReference(String transactionReference) {
+        this.transactionReference = transactionReference;
+    }
+
+
+}
