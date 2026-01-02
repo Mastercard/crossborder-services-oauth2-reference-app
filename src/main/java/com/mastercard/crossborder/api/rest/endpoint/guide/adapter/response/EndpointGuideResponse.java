6c990cabed9600b1885e7fecb854017fe67ad5ed
snehalubale <108735539+snehalubale@users.noreply.github.com>
2026-01-02 15:16:14 +0530

    Create EndpointGuideResponse.java

---
+++ src/main/java/com/mastercard/crossborder/api/rest/endpoint/guide/adapter/response/EndpointGuideResponse.java
@@ -0,0 +1,27 @@
+package com.mastercard.crossborder.api.rest.endpoint.guide.adapter.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mastercard.crossborder.api.rest.endpoint.guide.adapter.response.begapi.Business;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.SneakyThrows;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class EndpointGuideResponse extends BaseResponse {
+    private PaymentType paymentType;
+    private String destinationCountry;
+    private String destinationCurrency;
+    private DestinationPaymentInstrument destinationPaymentInstrument;
+    private Technical technical;
+    private Business business;
+
+    @SneakyThrows
+    @Override
+    public String toString() {
+        return new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_EMPTY).writeValueAsString(this);
+    }
+}
+
