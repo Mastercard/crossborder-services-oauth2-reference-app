50ff765bb2a3f732473430649718a0a2c015d82d
snehalubale <108735539+snehalubale@users.noreply.github.com>
2026-03-13 18:41:00 +0530

    Refactor import statements in EndpointGuideAdapterApiTest

--- src/test/java/com/mastercard/crossborder/endpoint/guide/adapter/adapter/EndpointGuideAdapterApiTest.java
+++ src/test/java/com/mastercard/crossborder/endpoint/guide/adapter/adapter/EndpointGuideAdapterApiTest.java
@@ -1,10 +1,10 @@
 package com.mastercard.crossborder.api.endpoint.guide.adapter.api;
 
 import com.mastercard.crossborder.api.config.MastercardApiConfig;
-import com.mastercard.crossborder.api.endpoint.guide.adapter.api.helper.EndpointGuideAdapter;
+import com.mastercard.crossborder.api.rest.endpoint.guide.adapter.api.helper.EndpointGuideAdapter;
 import com.mastercard.crossborder.api.exception.ServiceException;
-import com.mastercard.crossborder.api.rest.endpoint.guide.adapter.api.EndpointGuideAdapterApi;
-import com.mastercard.crossborder.api.rest.endpoint.guide.adapter.api.response.EndpointGuideResponse;
+import com.mastercard.crossborder.api.rest.endpoint.guide.adapter.EndpointGuideAdapterApi;
+import com.mastercard.crossborder.api.rest.endpoint.guide.adapter.response.EndpointGuideResponse;
 import com.mastercard.crossborder.api.rest.response.Error;
 import com.mastercard.crossborder.api.rest.response.Errors;
 import org.junit.Assert;
