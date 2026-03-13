eb2646762e63b903f3c6ca7caeef6704f5e31a2b
snehalubale <108735539+snehalubale@users.noreply.github.com>
2026-03-13 18:50:19 +0530

    Fix package declaration in EndpointGuideAdapterApiTest

--- src/test/java/com/mastercard/crossborder/endpoint/guide/adapter/adapter/EndpointGuideAdapterApiTest.java
+++ src/test/java/com/mastercard/crossborder/endpoint/guide/adapter/adapter/EndpointGuideAdapterApiTest.java
@@ -1,7 +1,7 @@
-package com.mastercard.crossborder.api.endpoint.guide.adapter;
+package com.mastercard.crossborder.api.endpoint.guide.adapter.adapter;
 
 import com.mastercard.crossborder.api.config.MastercardApiConfig;
-import com.mastercard.crossborder.api.rest.endpoint.guide.adapter.helper.EndpointGuideAdapter;
+import com.mastercard.crossborder.api.rest.endpoint.guide.adapter.adapter.helper.EndpointGuideAdapter;
 import com.mastercard.crossborder.api.exception.ServiceException;
 import com.mastercard.crossborder.api.rest.endpoint.guide.adapter.EndpointGuideAdapterApi;
 import com.mastercard.crossborder.api.rest.endpoint.guide.adapter.response.EndpointGuideResponse;
